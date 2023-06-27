6c86927b675e13ea523d4ee6534ad076021f057a
LaraKinan <lara.kinan13@gmail.com>
2023-06-27 23:52:33 +0300

    Fixing bug

--- code/myLoadBalancer.py
+++ code/myLoadBalancer.py
@@ -70,8 +70,8 @@ def expectedTime(servID, reqType, reqTime):
 
 def expectedTotalTime(servID, reqType, reqTime, reqRecvTime):
     times = []
-    start_time = reqRecvTime if serverTimes['serv%d' % i][1] == 0 else serverTimes['serv%d' % i][1]
     for i in range(1, len(servers) + 1):
+        start_time = reqRecvTime if serverTimes['serv%d' % i][1] == 0 else serverTimes['serv%d' % i][1]
         if i == servID:
             times.append( start_time + expectedTime(i, reqType, reqTime))
         else:
